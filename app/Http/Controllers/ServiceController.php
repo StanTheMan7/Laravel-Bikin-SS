f0ce4059299350e00b02895d50166ec868e5214f
StanTheMan7 <sts.graur@gmail.com>
2021-09-30 15:15:31 +0200

    derniere controller

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Service;
 use Illuminate\Http\Request;
+use Illuminate\View\ViewName;
 
 class ServiceController extends Controller
 {
@@ -14,7 +15,8 @@ class ServiceController extends Controller
      */
     public function index()
     {
-        
+        $services = Service::all();
+        return view('backoffice.serviceSection.all', compact('services'));
     }
 
     /**
@@ -24,7 +26,7 @@ class ServiceController extends Controller
      */
     public function create()
     {
-        //
+        return  view('backoffice.serviceSection.create');
     }
 
     /**
@@ -35,7 +37,19 @@ class ServiceController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
+        request()->validate([
+            "icon"=>["required"],
+            "title"=>["required", "min:1", "max:500"],
+            "text"=>["required"],
+        ]);
+
+            $service = new Service();
+            $service->icon = $request->icon;
+            $service->title = $request->title;
+            $service->text = $request->text;
+            $service->save();
+        return redirect()->route("service.index");
     }
 
     /**
@@ -46,7 +60,7 @@ class ServiceController extends Controller
      */
     public function show(Service $service)
     {
-        //
+        return  view('backoffice.serviceSection.show');
     }
 
     /**
@@ -57,7 +71,7 @@ class ServiceController extends Controller
      */
     public function edit(Service $service)
     {
-        //
+        return  view('backoffice.serviceSection.show');
     }
 
     /**
@@ -69,7 +83,17 @@ class ServiceController extends Controller
      */
     public function update(Request $request, Service $service)
     {
-        //
+        request()->validate([
+            "icon"=>["required"],
+            "title"=>["required", "min:1", "max:500"],
+            "text"=>["required"],
+        ]);
+
+            $service->icon = $request->icon;
+            $service->title = $request->title;
+            $service->text = $request->text;
+            $service->save();
+        return redirect()->route("service.index");
     }
 
     /**
@@ -80,6 +104,7 @@ class ServiceController extends Controller
      */
     public function destroy(Service $service)
     {
-        //
+        $service->delete();
+        return redirect()->route('service.index');
     }
 }
